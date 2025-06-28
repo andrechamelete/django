da8c9eb35005e43ff0fbb4a3c6fa45b713becf4a
André Chamelete <chamelete@gmail.com>
2025-06-28 16:31:35 -0300

    learning urls.py routing and views.py

--- projeto1/demoapp/urls.py
+++ projeto1/demoapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('home/', views.home),
+    path('homepage/', views.homepage),
+    path('date/', views.date),
+    path('menu/', views.menu)
 ]
--- projeto1/demoapp/views.py
+++ projeto1/demoapp/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
-def home(request):
+def home(request): 
+    path = request.path 
+    method = request.method 
+    name = "Andre"
+    content=''' 
+<center><h2>Testing Django Request Response Objects</h2> 
+<p>Request path : " {}</p> 
+<p>Request Method :{}</p>
+<p>nome: {}</p></center> 
+'''.format(path, method, name) 
+    return HttpResponse(content) 
+
+def homepage(request):
     return HttpResponse('<h1>Welcome to Little Lemon</h1>')
+
+def date(request):
+    date_joined = datetime.today().month
+    return HttpResponse(date_joined)
+
+def menu(request):
+    text = """<h1 style="color: #F4CE14;">This is Little Lemon again</h1>"""
+    return HttpResponse(text)
